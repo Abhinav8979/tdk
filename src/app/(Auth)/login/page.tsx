fce4d768b6994807f51176907f34a56a30cc7d96
Abhinav8979 <virusfound899@gmail.com>
2025-11-19 17:07:27 +0530

    working

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -19,41 +19,43 @@ export default async function LoginPage({ searchParams }: Props) {
 
   return (
     <>
-      <div className="absolute inset-0 w-fit h-fit">
-        <Image
-          src="/logo.png"
-          width={200}
-          height={200}
-          alt="Avea"
-          priority
-          loading="eager"
-        />
-      </div>
       <div
         style={{ background: "var(--background-gradient)" }}
-        className="flex min-h-screen items-center justify-center px-4 py-12"
+        className="h-screen flex flex-col items-start justify-center overflow-hidden px-4 py-12"
       >
-        <div className="flex flex-col md:flex-row w-full max-w-6xl backdrop-blur-sm min-h-[400px] gap-8">
-          {/* Login Form */}
-          <div className="w-full md:w-1/2 p-8 md:p-12 flex flex-col justify-center bg-white rounded-lg shadow-xl border-2 border-[var(--primary-background)]/60">
-            <h2 className="text-4xl font-extrabold text-gray-800 mb-2">
-              Welcome to HR Portal
-            </h2>
-            <p className="text-gray-500 mb-8">
-              Enter your credentials to access your account.
-            </p>
-            <LoginForm />
-          </div>
+        <div className="w-fit h-fit">
+          <Image
+            src="/logo.png"
+            width={200}
+            height={200}
+            alt="Avea"
+            priority
+            loading="eager"
+          />
+        </div>
+        <div className="flex items-center justify-center w-full mt-5 px-4">
+          <div className="flex flex-col  md:flex-row w-full max-w-6xl backdrop-blur-sm min-h-[400px] gap-4">
+            {/* Login Form */}
+            <div className="w-full md:w-1/2 p-8 md:p-12 flex flex-col justify-center bg-white rounded-lg shadow-xl border-2 border-[var(--primary-background)]/60">
+              <h2 className="text-4xl font-extrabold text-gray-800 mb-2">
+                Welcome to HR Portal
+              </h2>
+              <p className="text-gray-500 mb-8">
+                Enter your credentials to access your account.
+              </p>
+              <LoginForm />
+            </div>
 
-          <div className="hidden md:flex w-full md:w-1/2 items-center justify-center">
-            <Image
-              src={LoginImage.path}
-              alt={LoginImage.alt}
-              className="w-full h-auto max-w-[85%]"
-              width={LoginImage.width}
-              height={LoginImage.height}
-              priority
-            />
+            <div className="hidden md:flex w-full md:w-1/2 items-center justify-center">
+              <Image
+                src={LoginImage.path}
+                alt={LoginImage.alt}
+                className="w-full h-auto max-w-[85%]"
+                width={LoginImage.width}
+                height={LoginImage.height}
+                priority
+              />
+            </div>
           </div>
         </div>
       </div>
